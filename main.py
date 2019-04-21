15c6e6053113bc9be12749f04e84c643c46c57a5
seantyh <seantyh@gmail.com>
2019-04-21 14:35:35 +0800

    refactor main.py

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import argparse
 from pathlib import Path
-from MWE2019.scripts import script_variations
-from MWE2019.scripts import script_cwnvectors
-from MWE2019.scripts import script_cwnmorphgraph
-from MWE2019.scripts import script_variations
-from MWE2019.scripts import script_build_unigram
-from MWE2019.scripts import script_enclosing_ngrams
 
 parser = argparse.ArgumentParser("MWE2019 command line interface")
 parser.add_argument('-t', '--task', default='variation', help="task to run",
@@ -17,16 +11,26 @@ parser.add_argument('--sample-ratio', default=1, type=float, help="sample ratio 
 args = parser.parse_args()
 
 
-if args.task == "variations":
+if args.task == "variations":   
+    from MWE2019.scripts import script_variations
     script_variations(**vars(args))
+
 elif args.task == "cwnvectors":
+    from MWE2019.scripts import script_cwnvectors
     script_cwnvectors(**vars(args))
+
 elif args.task == "morphgraph":
+    from MWE2019.scripts import script_cwnmorphgraph
     script_cwnmorphgraph(**vars(args))
+
 elif args.task == "unigram":
+    from MWE2019.scripts import script_build_unigram
     script_build_unigram(**vars(args))
+
 elif args.task == "enclosing":
+    from MWE2019.scripts import script_enclosing_ngrams
     script_enclosing_ngrams(**vars(args))
+    
 else:
     print("task not recognized")
 
