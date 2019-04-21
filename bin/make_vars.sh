6070c3d28769dd3eeec76c397473960e31c4d45a
seantyh <seantyh@gmail.com>
2019-04-21 14:32:22 +0800

    update make_vars.sh

--- bin/make_vars.sh
+++ bin/make_vars.sh
@@ -1,14 +1,14 @@
-# !/bin/bash
+#!/bin/bash
 
 PWD=$(pwd)
 source /home/seantyh/langon/venv/bin/activate
 SCRIPT_DIR=`dirname $0`
 cd "$SCRIPT_DIR/../"
-python main.py --task variation --corpus apple --sample-ratio 0.01
+python main.py --task variations --corpus apple
 mkdir -p data/variations
 docker exec mwe_mongo bash \
     -c 'exec mongoexport -d mwe -c apple --pretty' \
-    > data/variations/apple_vars.json
+    > data/variations/apple_variations.json
 cd $PWD
 
 
