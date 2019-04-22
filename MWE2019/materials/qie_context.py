80461aecc60313270722a67e735661505d47ee4b
seantyh <seantyh@gmail.com>
2019-04-22 17:08:32 +0800

    compute exact_frequency in qie_list.ipynb

--- MWE2019/materials/qie_context.py
+++ MWE2019/materials/qie_context.py
@@ -1,34 +1,22 @@
 import re
-from collections import Counter
-from itertools import chain
+from collections import defaultdict
 from typing import Iterable
 import numpy as np
+from ..utils import tqdm
 from ..corpus import CorpusArticles, Article
 from ..corpus_index import CorpusIndex
 
 class QieContext:
-    def __init__(self, 
-            corpus_articles: CorpusArticles, 
-            corpus_index: CorpusIndex):
+    def __init__(self,
+            corpus_articles: CorpusArticles):
         self.articles = corpus_articles
-        self.corpus_index = corpus_index        
-    
-    def search_context(self, ngram):
-        context = []
-        try:                            
-            hit_indices = self.corpus_index.search_all_of(ngram)            
-            hit_iter = [self.articles[art_i] for art_i in hit_indices]
-            mat_context = self.search_sentence(ngram, hit_iter)
-            context.extend(mat_context)
-        except Exception as ex:
-            print(ex)
-        return context
 
-    def search_sentence(self, ngram: str, hit_iter: Iterable[Article]) -> Counter:
-        ctx_list = []        
-        sent_delim = re.compile("[。！？!?\r\n]")
-        for _, _, article_text in hit_iter:
+    def search_context(self, ngrams):
+        context = defaultdict(list)
+        sent_delim = re.compile("[。！？!?\r\n]")        
+        for _, _, article_text in tqdm(self.articles):
             sentences = sent_delim.split(article_text)
-            matches = [x for x in sentences if ngram in x]            
-            ctx_list.extend(matches)
-        return ctx_list
+            for ngram_x in ngrams:
+                matches = [sent_x for sent_x in sentences if ngram_x in sent_x]
+                context[ngram_x].extend(matches)            
+        return context
